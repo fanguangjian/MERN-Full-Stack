931b9f4d5592d2249e4f328872cecba4fa26e28f
Grant <fangj@GrantdeMacBook-Pro.local>
2021-07-24 00:17:15 +1000

    react init

---
+++ mern-cloudmel-frontend/client/src/App.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: G.F
+ * @Date: 2021-07-24 00:08:10
+ * @LastEditTime: 2021-07-24 00:14:33
+ * @LastEditors: your name
+ * @Description: 
+ * @FilePath: /MERN-Full-Stack/mern-cloudmel-frontend/client/src/App.js
+ */
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mern CloudMel</h1>
+    </div>
+  );
+}
+
+export default App;
