fdaaf690c71b623337ab6d8380b16b5532a0555b
Grant <fangj@GrantdeMacBook-Pro.local>
2021-07-24 10:52:01 +1000

    config less

--- mern-cloudmel-frontend/client/src/App.js
+++ mern-cloudmel-frontend/client/src/App.js
@@ -8,11 +8,15 @@
  */
 import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+// import './styles.less';
 
 function App() {
   return (
     <div className="App">
-      <h1>Mern CloudMel</h1>
+      {/* <h1>Mern CloudMel</h1> */}
+      <Navbar></Navbar>
     </div>
   );
 }
